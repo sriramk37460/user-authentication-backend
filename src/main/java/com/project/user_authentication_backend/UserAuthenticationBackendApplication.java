4855e3e8872c86b37e49226bd7e152d2b022b132
sriram <srir37460@gmail.com>
2024-10-22 20:08:24 +0530

    initial setup

---
+++ src/main/java/com/project/user_authentication_backend/UserAuthenticationBackendApplication.java
@@ -0,0 +1,13 @@
+package com.project.user_authentication_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserAuthenticationBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserAuthenticationBackendApplication.class, args);
+	}
+
+}
