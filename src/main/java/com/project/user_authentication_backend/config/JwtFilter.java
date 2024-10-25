3d21c03c7901763a1cb4663f9bf5bdb2bdfdf639
sriram <srir37460@gmail.com>
2024-10-25 23:33:56 +0530

    add config files

---
+++ src/main/java/com/project/user_authentication_backend/config/JwtFilter.java
@@ -0,0 +1,56 @@
+package com.project.user_authentication_backend.config;
+
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class JwtFilter extends OncePerRequestFilter {
+
+    private static final Logger logger = LoggerFactory.getLogger(JwtFilter.class);
+
+    @Autowired
+    private JwtTokenProvider jwtTokenProvider;
+    @Autowired
+    private CUOConfig cuoConfig;
+    @Autowired
+    private CustomUserDetailsService userDetailsService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        String token = resolveToken(request);
+        logger.debug("Token resolved: {}", token);
+        if (token != null && jwtTokenProvider.validateToken(token)) {
+            int userId = Integer.parseInt(jwtTokenProvider.extractUserId(token));
+            cuoConfig.setUserId(userId);
+            logger.debug("Token is valid. User ID: {}", userId);
+            UserDetails userDetails = userDetailsService.loadUserById(userId);
+            Authentication authentication = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+        filterChain.doFilter(request, response);
+    }
+
+    private String resolveToken(HttpServletRequest request) {
+        String bearerToken = request.getHeader("Authorization");
+        if (bearerToken != null && bearerToken.startsWith("Bearer ")) {
+            return bearerToken.substring(7);
+        }
+        return null;
+    }
+
+}
---
+++ src/main/java/com/project/user_authentication_backend/config/JwtTokenProvider.java
@@ -0,0 +1,117 @@
+package com.project.user_authentication_backend.config;
+
+import com.myproject.userauthentication.dao.UserRepository;
+import com.myproject.userauthentication.entity.User;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.temporal.TemporalAdjusters;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Optional;
+import java.util.function.Function;
+
+@Component
+public class JwtTokenProvider {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Value("${SALT_SECRET}")
+    private String SECRET_KEY;
+
+    private Date calculateNextMonthEndDate(){
+        LocalDate endOfMonth = LocalDate.now().with(TemporalAdjusters.lastDayOfMonth());
+        return Date.from(endOfMonth.atStartOfDay(ZoneId.systemDefault()).toInstant());
+    }
+
+    private final Date VALIDITY_IN_MONTHS = calculateNextMonthEndDate();
+
+    public String createToken(int userId,String userName,String role){
+        Date now = new Date();
+        Calendar calendar =Calendar.getInstance();
+        calendar.setTime(now);
+        calendar.add(Calendar.YEAR,10);
+        Date adminExpiration = calendar.getTime();
+        Claims claims=Jwts.claims()
+                .setIssuer(userName)
+                .setSubject(Integer.toString(userId))
+                .setIssuedAt(now);
+
+        claims.put("userName",userName);
+        claims.put("userId",Integer.toString(userId));
+        claims.put("role",role);
+
+        Optional<User> user= userRepository.findById(userId);
+
+        if(user.isPresent()){
+            if(user.get().isAdmin())
+            {
+                claims.setExpiration(adminExpiration);
+            }
+            else {
+                claims.setExpiration(VALIDITY_IN_MONTHS);
+            }
+        }
+        return Jwts.builder().setClaims(claims).signWith(SignatureAlgorithm.HS256,SECRET_KEY).compact();
+    }
+
+    public Claims extractAllClaims(String token) {
+        return Jwts.parser()
+                .setSigningKey(SECRET_KEY)
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        System.out.println("EXTRACT ..");
+        return claimsResolver.apply(claims);
+    }
+
+    // Extract user ID from token
+    public String extractUserId(String token) {
+        if(token != null && token.startsWith("Bearer "))
+        {
+            token = token.substring(7);
+        }
+        System.out.println("EXTRACT USER ID..");
+        return extractClaim(token, claims -> claims.get("userId", String.class));
+    }
+
+    // Extract role from token
+    public String extractRole(String token) {
+        return extractClaim(token, claims -> claims.get("role", String.class));
+    }
+
+    // Extract username (issuer) from token
+    public String extractUsername(String token) {
+        return extractClaim(token, claims -> claims.get("userName", String.class));
+    }
+
+    public String getCurrentUsername() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        System.out.println("authenticate");
+        if (authentication != null) {
+            return authentication.getName();
+        }
+        return null;
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parser().setSigningKey(SECRET_KEY).parseClaimsJws(token);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+}
