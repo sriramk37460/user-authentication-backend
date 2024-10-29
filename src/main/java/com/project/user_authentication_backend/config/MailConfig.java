645560518299975d9d3a030705891db8901f0c44
sriram <srir37460@gmail.com>
2024-10-29 19:32:49 +0530

    add get request user list

---
+++ src/main/java/com/project/user_authentication_backend/config/MailConfig.java
@@ -0,0 +1,38 @@
+package com.project.user_authentication_backend.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+@Configuration
+public class MailConfig {
+
+    @Value("${MAIN_EMAIL}")
+    private String SECRET_EMAIL;
+
+    @Value("${MAIN_PASSWORD}")
+    private String SECRET_PASSWORD;
+
+    @Bean
+    public JavaMailSender javaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("smtp.gmail.com"); // Replace with your SMTP host
+        mailSender.setPort(587);
+
+        mailSender.setUsername(SECRET_EMAIL); // Replace with your email
+        mailSender.setPassword(SECRET_PASSWORD); // Replace with your email password
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.debug", "true");
+
+        return mailSender;
+    }
+}
+
